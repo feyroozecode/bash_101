eb8331d1afd9218bdbf38ace215df12b938dac7f
ibrahmad <ibrahmad18@gmail.com>
2024-02-23 08:43:10 +0100

    adding 01_get_user_info

---
+++ 01_get_user_info.sh
@@ -0,0 +1,23 @@
+#!/usr/local/bin/bash
+
+echo "What's your name : "
+
+read name 
+
+echo "Salam Aleykoum $name!!"
+
+sleep 1
+
+echo "Enter your age : "
+
+read age
+
+sleep 1 
+
+echo "You're $age old Mr $name that cool"
+
+sleep 1 
+
+echo "Good by "
+
+sleep 1
