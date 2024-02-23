56af28acfef3e1097220d1eee377d8a4a064cb70
ibrahmad <ibrahmad18@gmail.com>
2024-02-23 08:33:01 +0100

    First screen

---
+++ first_script.sh
@@ -0,0 +1,28 @@
+#!/usr/local/bin/bash
+
+echo "Salam Mom"
+
+sleep 1
+
+echo "How are you"
+
+sleep 2 
+
+echo  "A bientot, Salam Aleykoum"
+
+sleep 2
+
+echo "exit program "
+
+sleep 2
+
+echo "2"
+
+sleep 1 
+
+echo "1"
+
+sleep 1
+
+echo "0"
+
